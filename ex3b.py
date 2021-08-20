f3e67bffc377e2d1819a0f18366eed1bfaf0a37b
Sekhar <sekhar@test>
2021-08-20 13:58:08 +0530

    python progrm
    "
    "

---
+++ ex3b.py
@@ -0,0 +1,6 @@
+''' control flow
+using a for loop,write a program that prints out the decimal equivalents of 1/2,1/3,1/4....1/10'''
+
+
+for i in range(1, 11):
+	print "1/" + str(i) + " is " + str(1.0/i)
---
+++ ex3d.py
@@ -0,0 +1,10 @@
+''' control flow
+write a program using a while loop that asks the user for a number ,prints a countdown 
+from that number to zero'''
+
+
+
+number = input("enter the number")
+while (number >= 0):
+   print 'The count is:', number
+   number = number - 1
