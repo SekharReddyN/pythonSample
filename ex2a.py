c975d2740954e252e4d24fc1bfbe7619f5d3c021
Sekhar <sekhar@test.com>
2021-08-20 13:33:25 +0530

    python program

---
+++ ex2a.py
@@ -0,0 +1,10 @@
+'''exercise 2
+a)write a program to compute distance between two points taking input from the user
+pythagorean theorem'''
+\pythagorean theorem is A^2+B^2=c^2
+import math
+x = input("enter the first point :")
+y = input("enter the second point :")
+z = (x**2+y**2)
+squt =z**0.5
+print "The distance between two points is :",squt
