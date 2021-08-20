8f886722056220c4958a3a3e2b3485d4013c7d4f
Sekhar <sekhar@test.com>
2021-08-20 13:45:29 +0530

    python progrm

---
+++ ex3a.py
@@ -0,0 +1,8 @@
+'''control flow
+write a program for checking whether the given number is even or not'''
+
+num=input("enter the number:")
+if (num%2) ==0:
+	print num,"is even number"
+else:
+	print num,"is not even number " 
